8c0b235ef628c807a5cb44f8937bd6e5b7ffe16e
Yang-Yiming <57488194+Yang-Yiming@users.noreply.github.com>
2024-11-30 00:37:49 +0800

    bug

--- src/main/java/org/model/MapMatrix.java
+++ src/main/java/org/model/MapMatrix.java
@@ -1,24 +1,99 @@
 package org.model;
 
-public class MapMatrix {
-    private int[][] matrix; // 二进制下， 第0位表示是否有墙，第1位是否有箱子，第2位是否有玩家，第3位是否有goal
-    private int[][] box_matrix;
+import java.util.HashMap;
 
+public class MapMatrix {
+
+    HashMap<Vector, Integer> matrix = new HashMap<>(); // 二进制下， 第0位表示是否有墙，第1位是否有箱子，第2位是否有玩家，第3位是否有goal
+    HashMap<Vector, Integer> box_matrix = new HashMap<>();
+
+    private int left_boundary=0, right_boundary=0, up_boundary=0, down_boundary=0;
+
+    private int DEFAULT_VALUE;
+
+    public void set(int x, int y, int value) {
+        if(value == 0){
+            matrix.remove(new Vector(x, y));
+            return;
+        }
+        matrix.put(new Vector(x, y), value);
+        if(x < left_boundary) left_boundary = x;
+        if(x > right_boundary) right_boundary = x;
+        if(y < up_boundary) up_boundary = y;
+        if(y > down_boundary) down_boundary = y;
+    }
+    public int get(int x, int y) {
+        return matrix.getOrDefault(new Vector(x, y), DEFAULT_VALUE);
+    }
+
+    public void set_data(int begin_x, int begin_y, int[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                set(begin_x + j, begin_y + i, data[i][j]);
+            }
+        }
+    }
+
+    public void add_data(int begin_x, int begin_y, int[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if(data[i][j] != DEFAULT_VALUE) {
+                    set(begin_x + j, begin_y + i, data[i][j]);
+                }
+            }
+        }
+    }
+
+    public void delete(int x, int y) {
+        matrix.remove(new Vector(x, y));
+    }
+
+    public void clear(int begin_x, int begin_y, int end_x, int end_y) {
+        for (int i = begin_x; i <= end_x; i++) {
+            for (int j = begin_y; j <= end_y; j++) {
+                delete(i, j);
+            }
+        }
+    }
+
+    public void clearAll() {
+        matrix.clear();
+    }
+
+    // 加入一条道路
+    public void add_road(int begin_x, int begin_y, int width, int length, Direction direction) {
+        if(direction == Direction.LEFT) {
+            for(int i = begin_x; i > begin_x - length; i--) {
+                set(i, begin_y, 1); set(i, begin_y + width - 1, 1);
+            }
+        } else if (direction == Direction.RIGHT) {
+            for(int i = begin_x; i < begin_x + length; i++) {
+                set(i, begin_y, 1); set(i, begin_y + width - 1, 1);
+            }
+        } else if (direction == Direction.UP) {
+            for(int i = begin_y; i > begin_y - length; i--) {
+                set(begin_x, i, 1); set(begin_x + width - 1, i, 1);
+            }
+        } else if (direction == Direction.DOWN) {
+            for(int i = begin_y; i < begin_y + length; i++) {
+                set(begin_x, i, 1); set(begin_x + width - 1, i, 1);
+            }
+        }
+    }
+
+    // 与正常map一样的部分
     public boolean isOne(int num, int n) {
         return (((num >> n) & 1) == 1);
     }
 
     public boolean hasNothing(int x, int y) {
-        return matrix[y][x] == 0;
+        return this.get(x, y) == 0;
     }
 
     public boolean hasNoObstacle(int x, int y) {
-        if(x < 0 || x >= this.getWidth() || y < 0 || y >= this.getHeight()) {
-            return true;
-        }
         int[] ObstacleTypes = { 0, 1, 2 }; // wall box player 会阻挡
         for (int e : ObstacleTypes) {
-            if (isOne(this.matrix[y][x], e)) {
+            if (isOne(get(x,y), e)) {
                 return false;
             }
         }
@@ -26,67 +101,84 @@ public class MapMatrix {
     }
 
     public boolean hasWall(int x, int y) {
-        return isOne(matrix[y][x], 0);
+        return isOne(get(x,y), 0);
     }
 
     public boolean hasBox(int x, int y) {
-        return isOne(matrix[y][x], 1);
+        return isOne(get(x,y), 1);
     }
 
     public boolean hasPlayer(int x, int y) {
-        return isOne(matrix[y][x], 2);
+        return isOne(get(x,y), 2);
     }
 
     public boolean hasGoal(int x, int y) {
-        return isOne(matrix[y][x], 3);
-    }
-
-    public void set(int x, int y, int num) {
-        matrix[y][x] = num;
+        return isOne(get(x,y), 3);
     }
 
     public void add(int x, int y, int type) {
-        matrix[y][x] += (1 << type);
+        set(x,y,get(x,y) + (1 << type));
     }
 
     public void remove(int x, int y, int type) {
-        matrix[y][x] -= (1 << type);
+        set(x,y,get(x,y)- (1 << type));
     }
 
     public void setBox_matrix(int x, int y, int num) {
-        box_matrix[y][x] = num;
+        box_matrix.put(new Vector(x, y), num);
     }
     public int getBox_matrix_id(int x, int y) {
-        return box_matrix[y][x];
-    }
-
-    public int getWidth() {
-        return this.matrix[0].length;
-    }
-
-    public int getHeight() {
-        return this.matrix.length;
+        return box_matrix.getOrDefault(new Vector(x, y), 0);
     }
 
     public int[][] getMatrix() {
-        return matrix;
+        int[][] ans = new int[getHeight()][getWidth()];
+        for (int y = up_boundary; y < down_boundary; ++y) {
+            for (int x = left_boundary; x < right_boundary; ++x) {
+                ans[y - up_boundary][x - left_boundary] = get(x, y);
+            }
+        }
+        return ans;
+    }
+
+    public int getWidth() {
+        int width = right_boundary - left_boundary;
+        return (int) Math.min(width, (double) config.ScreenWidth / config.tile_size * 1.2);
+    }
+
+    public int getHeight() {
+        int height = down_boundary - up_boundary;
+        return (int) Math.min(height, (double) config.ScreenHeight / config.tile_size * 1.2);
     }
 
     public MapMatrix(int[][] matrix) {
-        this.matrix = new int[matrix.length][matrix[0].length];
-        this.box_matrix = new int[matrix.length][matrix[0].length];
+        set_data(0,0,matrix);
+
+        box_matrix = new HashMap<>();
+
         int box_index = 1; // 编号从1开始
 
         for (int y = 0; y < matrix.length; ++y) {
             for (int x = 0; x < matrix[y].length; ++x) {
                 set(x, y, matrix[y][x]);
                 if(hasBox(x, y)) {
-                    box_matrix[y][x] = box_index++;
+                    setBox_matrix(x, y, box_index++);
                 } else {
-                    box_matrix[y][x] = 0;
+                    setBox_matrix(x, y, 0);
                 }
             }
         }
+    }
+
+    public MapMatrix() {
+        matrix = new HashMap<>();
+        box_matrix = new HashMap<>();
+        DEFAULT_VALUE = 0;
+    }
+    public MapMatrix(int DEFAULT_VALUE) {
+        matrix = new HashMap<>();
+        box_matrix = new HashMap<>();
+        this.DEFAULT_VALUE = DEFAULT_VALUE;
     }
 
 }
---
+++ src/main/java/org/test.java
@@ -0,0 +1,9 @@
+package org;
+
+import org.model.Vector;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new Vector(1, 2).equals(new Vector(1, 2)));
+    }
+}
