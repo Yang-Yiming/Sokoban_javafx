8b5fd5b890b4b628e89f7d4d174d8542a4cac71c
Yang-Yiming <57488194+Yang-Yiming@users.noreply.github.com>
2024-12-16 16:47:35 +0800

    mapdata

--- src/main/java/org/test.java
+++ src/main/java/org/test.java
@@ -1,5 +1,6 @@
 package org;
 
+import org.data.mapdata;
 import org.model.Coordinate;
 import org.model.MapMatrix;
 import org.model.Solve.Solve;
@@ -15,7 +16,7 @@ public class test {
         for(int i = 0; i < times; i++) {
             long startTime = System.currentTimeMillis();
             if(times == 1){
-                System.out.println(solve.aStarSearch());
+                System.out.println(solve.aStarSearch().equals("N")?"No solution":"Solution found");
             } else
                 solve.aStarSearch();
             total += System.currentTimeMillis() - startTime;
@@ -25,29 +26,8 @@ public class test {
 
 
     public static void main(String[] args) {
-//        Test(mapdata.maps[1], "EasyMap", 100);
-//        Test(mapdata.maps[4], "HardMap", 20);
-//        Test(new int[][] {
-//                        {1, 1, 1, 1, 1, 1, 0, 0},
-//                        {1, 0, 0, 0, 0, 1, 1, 1},
-//                        {1, 0, 0, 0, 8, 8, 0, 1},
-//                        {1, 2, 0, 2, 2, 4, 0, 1},
-//                        {1, 0, 0, 1, 0, 8, 0, 1},
-//                        {1, 1, 1, 1, 1, 1, 1, 1}},
-//                "NoSolveMap", 20);
-//        Solve solve = new Solve(new MapMatrix(mapdata.huge_maps[0]));
-        HashMap<Coordinate, Integer> map = new HashMap<>();
-        // 0 1 0 0
-        // 0 1 0 0
-        // 0 1 0 1
-        // 0 0 0 1
-        map.put(new Coordinate(1, 0), -1);
-        map.put(new Coordinate(1, 1), -1);
-        map.put(new Coordinate(1, 2), -1);
-        map.put(new Coordinate(3, 2), -1);
-        map.put(new Coordinate(3, 3), -1);
-
-        FindPath findPath = new FindPath(map, new Coordinate(0, 0), new Coordinate(2, 1));
-        System.out.println(findPath.findPath());
+        for(int i = 0; i < mapdata.maps2.length; i++) {
+            Test(mapdata.maps2[i], "map " + i, 10);
+        }
     }
 }
